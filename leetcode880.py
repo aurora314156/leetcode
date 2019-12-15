7158821b7f91e653d1874273588e9e06f3ebc074
aurora314156 <aurora314156@gmail.com>
2019-12-15 16:21:11 +0800

    leetcode 880 update.

--- leetcode880.py
+++ leetcode880.py
@@ -1,6 +1,20 @@
+"""
+Des: 880. Decoded String at Index
+Tags: Stack
+"""
+
 class Solution:
     def decodeAtIndex(self, S: str, K: int) -> str:
-        
-        
-        
-        return result
+        maxLen = 0
+        for i, c in enumerate(S):
+            maxLen = maxLen * int(c) if c.isdigit() else maxLen + 1
+            if K <= maxLen: break
+        for j in range(i, -1, -1):
+            char = S[j]
+            if char.isdigit():
+                maxLen /= int(char)
+                K %= maxLen
+            else:
+                if K == maxLen or not K: return char
+                maxLen -= 1
+       
--- tmp.py
+++ tmp.py
@@ -7,36 +7,25 @@ import pysnooper
 class Solution:
     @pysnooper.snoop()
     def decodeAtIndex(self, S: str, K: int) -> str:
-        N = 0
+        maxLen = 0
         for i, c in enumerate(S):
-            N = N * int(c) if c.isdigit() else N + 1
-            if K <= N: break
+            maxLen = maxLen * int(c) if c.isdigit() else maxLen + 1
+            if K <= maxLen: break
         for j in range(i, -1, -1):
-            c = S[j]
-            if c.isdigit():
-                N /= int(c)
-                K %= N
+            char = S[j]
+            if char.isdigit():
+                maxLen /= int(char)
+                K %= maxLen
             else:
-                if K == N or K == 0: return c
-                N -= 1
-        # tmpStr, curMaxLen = "", 1
-        # for char in S:
-        #     if char.isdigit(): 
-        #         tmpStr *= int(char)
-
-        #     else:
-        #         tmpStr += char
-        #     curMaxLen = len(tmpStr)
-        #     if curMaxLen >= K:
-        #         return tmpStr[ (K % len(tmpStr))-1]
+                if K == maxLen or not K: return char
+                maxLen -= 1
             
-#
-
 
 if __name__ == '__main__':
-    S, K = "leet2code3", 10
+    #S, K = "leet2code3", 10
+    S, K = "ha22", 5
     #S, K = "czjkk9elaqwiz7s6kgvl4gjixan3ky7jfdg3kyop3husw3fm289thisef8blt7a7zr5v5lhxqpntenvxnmlq7l34ay3jaayikjps", 768077956
     #S, K = "a2345678999999999999999", 1
     res = Solution().decodeAtIndex(S, K)
     
-    
+   
