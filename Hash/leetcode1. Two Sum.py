5a25afefe186e3ae8eab25573732f75d5a048081
aurora314156 <aurora314156@gmail.com>
2020-10-16 22:42:57 +0800

    1. update

---
+++ Hash/leetcode1. Two Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, v in enumerate(nums):
+            if target - v not in d:
+                d[v] = i
+            else:
+                return [d[target-v], i]
