935fa3624c95eecd5255d319a978735cbcc9ad06
aurora314156 <aurora314156@gmail.com>
2020-10-17 16:52:30 +0800

    905. update

---
+++ Array/leetcode905. Sort Array By Parity.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        if not A or len(A) == 1: return A
+        i, j  = 0, len(A)-1
+        while i < j: 
+            while i < j and A[i] % 2 == 0:
+                i += 1
+            while j > -1 and A[j] % 2 != 0: 
+                j -= 1
+            if i > j: break
+            tmp = A[i]
+            A[i] = A[j]
+            A[j] = tmp
+        return A
