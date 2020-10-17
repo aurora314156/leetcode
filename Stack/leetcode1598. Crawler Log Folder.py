cf8e8b3af8e097610a6a8736d2b55f185226b835
aurora314156 <aurora314156@gmail.com>
2020-10-17 17:57:23 +0800

    1598. update

---
+++ Stack/leetcode1598. Crawler Log Folder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        s = []
+        for l in logs:
+            if l[:2] == "..":
+                if len(s) == 0: continue
+                s.pop()
+            elif l[:2] == "./":
+                continue
+            else:
+                s.append(1)
+        return len(s)
