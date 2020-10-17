af0554723f0e1c45b7842d924841ce3dbf3a6da7
aurora314156 <aurora314156@gmail.com>
2020-10-17 16:26:57 +0800

    344. update

---
+++ Two pointer/leetcode344. Reverse String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l = len(s)-1
+        for i in range(l//2+1):
+            tmp = s[i]
+            s[i] = s[l-i]
+            s[l-i] = tmp
